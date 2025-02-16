4d385361563b40c62fa09febc789a0e1acf3ce2e
Rongeard Benoit <benoit.rongeard@gmail.com>
2025-02-16 23:14:23 +0100

    Adding env files + start working on register screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { Component, inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+// import { env } from 'process';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,12 @@ export class AppComponent {
   platform = inject(PLATFORM_ID);
 
   constructor() {
+    if (isPlatformServer(this.platform)) {
+      this.translateService.setDefaultLang(
+        process.env['LANG']?.substring(0, 2) ?? 'en'
+      );
+    }
+
     if (isPlatformBrowser(this.platform)) {
       this.translateService.setDefaultLang(
         navigator?.language?.substring(0, 2) ?? 'en'
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-register',
@@ -13,8 +15,24 @@ import { InputTextModule } from 'primeng/inputtext';
     ButtonModule,
     RouterLink,
     TranslateModule,
+    ReactiveFormsModule,
+    MessageModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  formBuilder = inject(FormBuilder);
+
+  registerForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+  });
+
+  register() {
+    this.registerForm.markAsDirty();
+    console.log('Register button clicked');
+    console.log(this.registerForm);
+  }
+}
